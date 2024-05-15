5093ac31b843d1e6939df37fd4caccbf41ed1b0c
abaddonpuff <abaddonpuff@gmail.com>
2024-05-15 14:05:56 -0400

    Fix for the X API

--- commandCenter/centralHub/xTwitter/twitterAPI.py
+++ commandCenter/centralHub/xTwitter/twitterAPI.py
@@ -1,16 +1,23 @@
 import os
-import requests                                                                                                                                                                                                     
+import requests 
+import json
+import config                      
+from requests_oauthlib import OAuth1Session                                                                                                                                                
 from dotenv import load_dotenv, find_dotenv
 from pathlib import Path
 
 load_dotenv()
 
 X_AUTHORIZATION_BEARER = os.getenv('X_BEARER_TOKEN')
+
+X_API_KEY = os.getenv('X_API_KEY')
+X_API_SECRET = os.getenv('X_API_SECRET')
+
 X_ACCESS_TOKEN = os.getenv('X_ACCESS_TOKEN')
-X_CLIENTID = os.getenv('CLIENT_ID')
-X_CLIENT_SECRET = os.getenv('CLIENT_SECRET')
 X_ACCESS_TOKEN_SECRET = os.getenv('X_ACCESS_TOKEN_SECRET')
-X_USERNAME_URL = 'https://api.twitter.com/2/users/by/username/'
+
+X_USERNAME_URL = 'https://api.twitter.com/2/users/me'
+X_TWEET_ID = 'https://api.twitter.com/2/tweets'
 X_AUTHENTICATION_URL = 'https://api.twitter.com/2/oauth2/token'
 
 def x_authentication(url):
@@ -29,13 +36,28 @@ def x_authentication(url):
     print(response.text)
 
 def x_authentication_authbearer():
-    headers = {
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Authorization': f'Bearer {X_AUTHORIZATION_BEARER}'
+
+    oauth = OAuth1Session(
+        X_API_KEY,
+        X_API_SECRET,
+        X_ACCESS_TOKEN,
+        X_ACCESS_TOKEN_SECRET
+    )
+
+    params = {
+    'usernames': '0xAbadd0n',
+    'user.fields': 'created_at',
+    'expansions': 'pinned_tweet_id',
+    'tweet.fields': 'author_id,created_at'
     }
 
-    user_url= X_USERNAME_URL + '0xAbadd0n'
-    response = requests.get(user_url, headers=headers)
+    user_url= X_USERNAME_URL
+
+    response = oauth.get(
+        user_url
+    )
+
+
     print(response.text)
 
 def main():
@@ -43,5 +65,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-    #M1M5R3BMVy13QmpScXkzTUt5OE46MTpjaQ
