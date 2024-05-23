d1db25c43df93e0ef1698b8099b6719a76b11032
abaddonpuff <abaddonpuff@gmail.com>
2024-05-23 10:07:40 -0400

    Changes on the tree done

--- commandCenter/centralHub/xTwitter/twitterAPI.py
+++ commandCenter/centralHub/xTwitter/twitterAPI.py
@@ -8,60 +8,55 @@ from pathlib import Path
 
 load_dotenv()
 
+#DEFINE AUTHENTICATION TOKENS
 X_AUTHORIZATION_BEARER = os.getenv('X_BEARER_TOKEN')
-
 X_API_KEY = os.getenv('X_API_KEY')
 X_API_SECRET = os.getenv('X_API_SECRET')
-
 X_ACCESS_TOKEN = os.getenv('X_ACCESS_TOKEN')
 X_ACCESS_TOKEN_SECRET = os.getenv('X_ACCESS_TOKEN_SECRET')
+#X_AUTHENTICATION_URL = 'https://api.twitter.com/2/oauth2/token'
 
-X_USERNAME_URL = 'https://api.twitter.com/2/users/me'
+#DEFINE URLS
+X_BASE_URL = 'https://api.twitter.com'
+X_GET_MYUSER = '/2/users/me'
+X_GET_USER = '/2/users/by/username/'
 X_TWEET_ID = 'https://api.twitter.com/2/tweets'
-X_AUTHENTICATION_URL = 'https://api.twitter.com/2/oauth2/token'
 
-def x_authentication(url):
-    
-    headers = {
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'code':'VGNibzFWSWREZm01bjN1N3dicWlNUG1oa2xRRVNNdmVHelJGY2hPWGxNd2dxOjE2MjIxNjA4MjU4MjU6MToxOmFjOjE',
-    'grant_type': 'authorization_code',
-    'client_id': X_CLIENTID,
-    'client_secret': X_CLIENT_SECRET,
-    'redirect_uri': 'https://127.0.0.1',
-    'code_verifier':'challenge'
-    }
 
-    response = requests.post(url, headers=headers)
-    print(response.text)
+def x_authenticate():
 
-def x_authentication_authbearer():
-
-    oauth = OAuth1Session(
+    return OAuth1Session(
         X_API_KEY,
         X_API_SECRET,
         X_ACCESS_TOKEN,
         X_ACCESS_TOKEN_SECRET
     )
 
-    params = {
-    'usernames': '0xAbadd0n',
-    'user.fields': 'created_at',
-    'expansions': 'pinned_tweet_id',
-    'tweet.fields': 'author_id,created_at'
-    }
-
-    user_url= X_USERNAME_URL
-
-    response = oauth.get(
-        user_url
-    )
+def x_userlookup(x_user=None):
+    x_session = x_authenticate()
 
 
-    print(response.text)
+    if x_user == None:
+        api_url = X_BASE_URL + X_GET_MYUSER
+        return x_session.get(api_url).text
+
+    else:
+        api_url = X_BASE_URL + X_GET_USER + x_user
+        #Define params
+        params = {
+            'user.fields': 'created_at',
+            'expansions': 'pinned_tweet_id',
+            'tweet.fields': 'author_id,created_at'
+        }
+
+        response = x_session.get(
+            api_url, params=params
+        )
+
+    return response.text
 
 def main():
-    x_authentication_authbearer()
+    print(x_userlookup("bbelderbos"))
 
 if __name__ == '__main__':
     main()
