1793f432d2c6022ec0934c7501a15fd1fb1b8e60
abaddonpuff <abaddonpuff@gmail.com>
2024-06-09 14:35:05 -0400

    issues when inserting into database

--- webCommander/centralHub/urls.py
+++ webCommander/centralHub/urls.py
@@ -3,11 +3,12 @@ from centralHub import views
 
 urlpatterns = [
     path('spotify/allArtists', views.list_artist, name="show_all_artists"),
-    path('spotify/submit_artist', views.get_artists, name="submit_artist"),
+    path('get_artists_form', views.get_artists_form, name="get_artists_form"),
+    path('spotify/submit_artist', views.submit_spotify_artist, name="submit_artist"),
     path('spotify/artists/<str:spotify_artist>', views.get_artists, name="artist_summary"),
+    path('artists',views.get_artists, name='get_artists'),
     path('x/allusers', views.list_twitter, name="showalltweets"),
     path('x/xHandles/<str:twitter_handle>', views.handle_summary, name="twitter_handle_summary"),
     path('x/submit_handle/', views.submit_x_user, name='submit_handle'),
     path('',views.home_page, name='home'),
-    path('artists',views.get_artists, name='get_artists'),
 ]
--- webCommander/centralHub/views.py
+++ webCommander/centralHub/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render,get_object_or_404
 from django.contrib import messages
-from centralHub.forms import SubmitXUser
+from centralHub.forms import SubmitXUser, SpotifySearchForm, SpotifyChoicesForm
 from centralHub.models import TwitterUser, TwitterUserPosts, SpotifyArtistInfo
 from centralHub.spotify.spotifyAPI import search_spotify_artist
 
@@ -41,6 +41,16 @@ def home_page(request):
 def list_artist(request):
     allartists = SpotifyArtistInfo.objects.all()
 
+    print(f'################MY REQUEST METHOD IS {request.method}')
+    if request.method == 'POST':
+        spotify_search_form = SpotifySearchForm(request.POST)
+        if spotify_search_form.is_valid():
+            artist_query = spotify_search_form.cleaned_data['artist']
+            return render(request, 'spotifyFramework/spotify_summary.html', {'artist_query':artist_query})
+    else:
+        print("####Im here")
+        spotify_search_form = SpotifySearchForm()
+
     return render(request, 'spotifyFramework/spotify_summary.html',{'allartists':allartists})
 
 def get_artists(request):
@@ -51,3 +61,21 @@ def get_artists(request):
         "<option value='{val}'>{val}, Popularity: {pop}</option>".format(val=row[0], pop=row[2]) for row in response
     )
     return HttpResponse(options)
+
+def get_artists_form(request):
+    print(f'################MY REQUEST METHOD IS {request.method}')
+    if request.method == 'POST':
+        spotify_search_form = SpotifySearchForm(request.POST)
+        if spotify_search_form.is_valid():
+            artist_query = spotify_search_form.cleaned_data['artist']
+            return render(request, 'spotifyFramework/spotify_summary.html', {'artist_query':artist_query})
+    else:
+        spotify_search_form = SpotifySearchForm()
+
+    return render(request, 'spotifyFramework/spotify_summary.html', {'spotify_search_form':spotify_search_form})
+
+def submit_spotify_artist(request):
+    if request.method == 'POST':
+        form_handle = SubmitSpotifyArtist(request.POST)
+        
+    return render(request, 'spotifyFramework/spotify_summary.html',{'form_handle':form_handle})
