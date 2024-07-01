e6e082db49363ca768a215cd8bbc217bdbe80fe2
abaddonpuff <abaddonpuff@gmail.com>
2024-06-30 21:37:06 -0400

    Changes

--- centralHub/urls.py
+++ centralHub/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('x/allusers', views.list_twitter, name="showalltweets"),
     path('x/xHandles/<str:twitter_handle>', views.handle_summary, name="twitter_handle_summary"),
     path('x/submit_handle/', views.submit_x_user, name='submit_handle'),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login')
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('',views.home_page, name='home'),
 ]
